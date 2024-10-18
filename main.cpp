45594f92d59c192f93105a2199f46447d6992a9f
Ilia Vasin <me@vasin-ilya.com>
2024-10-18 19:30:28 +0200

    image views done

--- main.cpp
+++ main.cpp
@@ -26,17 +26,18 @@ class HelloTriangleApplication
 	}
 
   private:
-	GLFWwindow          *window;
-	VkInstance           instance;
-	VkPhysicalDevice     physicalDevice;
-	VkDevice             device;
-	VkQueue              graphicsQueue;        // queue to the selected logical device
-	VkSurfaceKHR         surface;              // surface to render into
-	VkQueue              presentQueue;         // presentation qeueue, connected to the surface
-	VkSwapchainKHR       swapChain;
-	std::vector<VkImage> swapChainImages;
-	VkFormat             swapChainImageFormat;
-	VkExtent2D           swapChainExtent;
+	GLFWwindow              *window;
+	VkInstance               instance;
+	VkPhysicalDevice         physicalDevice;
+	VkDevice                 device;
+	VkQueue                  graphicsQueue;        // queue to the selected logical device
+	VkSurfaceKHR             surface;              // surface to render into
+	VkQueue                  presentQueue;         // presentation qeueue, connected to the surface
+	VkSwapchainKHR           swapChain;
+	std::vector<VkImage>     swapChainImages;
+	VkFormat                 swapChainImageFormat;
+	VkExtent2D               swapChainExtent;
+	std::vector<VkImageView> swapChainImageViews;
 
 	void mainLoop()
 	{
@@ -48,6 +49,10 @@ class HelloTriangleApplication
 
 	void cleanup()
 	{
+		for (auto imageView : swapChainImageViews)
+		{
+			vkDestroyImageView(device, imageView, nullptr);
+		}
 		vkDestroySwapchainKHR(device, swapChain, nullptr);
 		vkDestroyDevice(device, nullptr);
 		vkDestroySurfaceKHR(instance, surface, nullptr);
@@ -86,6 +91,7 @@ class HelloTriangleApplication
 		vkGetDeviceQueue(device, indices.presentFamily.value(), 0, &presentQueue);
 
 		createSwapChain();
+		createImageViews();
 	}
 
 	void createSurface()
@@ -260,6 +266,38 @@ class HelloTriangleApplication
 		vkGetSwapchainImagesKHR(device, swapChain, &imageCount, nullptr);
 		swapChainImages.resize(imageCount);
 		vkGetSwapchainImagesKHR(device, swapChain, &imageCount, swapChainImages.data());
+	}
+
+	void createImageViews()
+	{
+		swapChainImageViews.resize(swapChainImages.size());
+
+		for (int i; i++; i < swapChainImages.size())
+		{
+			VkImageViewCreateInfo createInfo{};
+			createInfo.sType    = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			createInfo.image    = swapChainImages[i];
+			createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			createInfo.format   = swapChainImageFormat;
+
+			// color settings
+			createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+			// image purpose??
+			createInfo.subresourceRange.aspectMask     = VK_IMAGE_ASPECT_COLOR_BIT;
+			createInfo.subresourceRange.baseMipLevel   = 0;
+			createInfo.subresourceRange.levelCount     = 1;
+			createInfo.subresourceRange.baseArrayLayer = 0;
+			createInfo.subresourceRange.layerCount     = 1;
+
+			if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS)
+			{
+				throw std::runtime_error("failed to create image views!");
+			}
+		}
 	}
 };
 
