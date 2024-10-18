bf3d3ba95072b7c42e4140824761553b82b92ea5
Ilia Vasin <me@vasin-ilya.com>
2024-10-18 17:20:16 +0200

    reformat

--- device_helpers.cpp
+++ device_helpers.cpp
@@ -20,150 +20,169 @@ const bool isMac = true;
 const bool isMac = false;
 #endif
 
-struct QueueFamilyIndices {
-    std::optional<uint32_t> graphicsFamily;
-    std::optional<uint32_t> presentFamily;
+struct QueueFamilyIndices
+{
+	std::optional<uint32_t> graphicsFamily;
+	std::optional<uint32_t> presentFamily;
 
-    bool isComplete() {
-        return graphicsFamily.has_value() && presentFamily.has_value();
-    }
+	bool isComplete()
+	{
+		return graphicsFamily.has_value() && presentFamily.has_value();
+	}
 };
 
-class DeviceHelpers {
-   public:
-    static QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device,
-                                                VkSurfaceKHR surface) {
-        QueueFamilyIndices indices;
+class DeviceHelpers
+{
+  public:
+	static QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device,
+	                                            VkSurfaceKHR     surface)
+	{
+		QueueFamilyIndices indices;
 
-        uint32_t queueFamilyCount = 0;
-        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount,
-                                                 nullptr);
+		uint32_t queueFamilyCount = 0;
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount,
+		                                         nullptr);
 
-        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
-        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount,
-                                                 queueFamilies.data());
+		std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount,
+		                                         queueFamilies.data());
 
-        int i = 0;
-        for (const auto& queueFamily : queueFamilies) {
-            if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
-                indices.graphicsFamily = i;
-            }
+		int i = 0;
+		for (const auto &queueFamily : queueFamilies)
+		{
+			if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+			{
+				indices.graphicsFamily = i;
+			}
 
-            VkBool32 presentSupport = false;
-            vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface,
-                                                 &presentSupport);
+			VkBool32 presentSupport = false;
+			vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface,
+			                                     &presentSupport);
 
-            if (presentSupport) {
-                indices.presentFamily = i;
-            }
+			if (presentSupport)
+			{
+				indices.presentFamily = i;
+			}
 
-            if (indices.isComplete()) {
-                break;
-            }
+			if (indices.isComplete())
+			{
+				break;
+			}
 
-            i++;
-        }
+			i++;
+		}
 
-        return indices;
-    }
+		return indices;
+	}
 
-    static std::tuple<bool, std::string> isDeviceSuitable(
-        VkPhysicalDevice device, VkSurfaceKHR surface) {
-        VkPhysicalDeviceProperties deviceProperties;
-        VkPhysicalDeviceFeatures deviceFeatures;
-        vkGetPhysicalDeviceProperties(device, &deviceProperties);
-        vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+	static std::tuple<bool, std::string> isDeviceSuitable(
+	    VkPhysicalDevice device, VkSurfaceKHR surface)
+	{
+		VkPhysicalDeviceProperties deviceProperties;
+		VkPhysicalDeviceFeatures   deviceFeatures;
+		vkGetPhysicalDeviceProperties(device, &deviceProperties);
+		vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
 
-        QueueFamilyIndices indices = findQueueFamilies(device, surface);
+		QueueFamilyIndices indices = findQueueFamilies(device, surface);
 
-        // originally there was deviceFeatures.geometryShader check, but it is
-        // not available in MacOS
-        bool isSuitable = indices.isComplete();
+		// originally there was deviceFeatures.geometryShader check, but it is
+		// not available in MacOS
+		bool isSuitable = indices.isComplete();
 
-        std::string name = std::string(deviceProperties.deviceName);
-        auto result = std::tuple(isSuitable, name);
+		std::string name   = std::string(deviceProperties.deviceName);
+		auto        result = std::tuple(isSuitable, name);
 
-        return result;
-    }
+		return result;
+	}
 
-    static VkPhysicalDevice pickPhysicalDevice(VkInstance instance,
-                                               VkSurfaceKHR surface) {
-        uint32_t deviceCount = 0;
-        vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+	static VkPhysicalDevice pickPhysicalDevice(VkInstance   instance,
+	                                           VkSurfaceKHR surface)
+	{
+		uint32_t deviceCount = 0;
+		vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
 
-        if (deviceCount == 0) {
-            throw std::runtime_error(
-                "failed to find GPUs with Vulkan support!");
-        }
+		if (deviceCount == 0)
+		{
+			throw std::runtime_error(
+			    "failed to find GPUs with Vulkan support!");
+		}
 
-        std::vector<VkPhysicalDevice> devices(deviceCount);
-        vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+		std::vector<VkPhysicalDevice> devices(deviceCount);
+		vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
 
-        for (const auto& device : devices) {
-            auto result = isDeviceSuitable(device, surface);
-            auto isSuitable = std::get<0>(result);
-            auto name = std::get<1>(result);
+		for (const auto &device : devices)
+		{
+			auto result     = isDeviceSuitable(device, surface);
+			auto isSuitable = std::get<0>(result);
+			auto name       = std::get<1>(result);
 
-            if (isSuitable) {
-                std::cout << std::string("Going to use ") + name +
-                                 std::string(" card\n");
-                return device;
-            }
-        }
+			if (isSuitable)
+			{
+				std::cout << std::string("Going to use ") + name +
+				                 std::string(" card\n");
+				return device;
+			}
+		}
 
-        throw std::runtime_error("failed to find a suitable GPU!");
-    }
+		throw std::runtime_error("failed to find a suitable GPU!");
+	}
 
-    static VkDevice createLogicalDevice(
-        VkPhysicalDevice physicalDevice,
-        std::vector<const char*> validationLayers, VkSurfaceKHR surface) {
-        QueueFamilyIndices indices = findQueueFamilies(physicalDevice, surface);
+	static VkDevice createLogicalDevice(
+	    VkPhysicalDevice          physicalDevice,
+	    std::vector<const char *> validationLayers, VkSurfaceKHR surface)
+	{
+		QueueFamilyIndices indices = findQueueFamilies(physicalDevice, surface);
 
-        VkDeviceQueueCreateInfo queueCreateInfo{};
-        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
-        queueCreateInfo.queueCount = 1;
+		VkDeviceQueueCreateInfo queueCreateInfo{};
+		queueCreateInfo.sType            = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+		queueCreateInfo.queueCount       = 1;
 
-        float queuePriority = 1.0f;
-        queueCreateInfo.pQueuePriorities = &queuePriority;
+		float queuePriority              = 1.0f;
+		queueCreateInfo.pQueuePriorities = &queuePriority;
 
-        // features, that Im going to use from videocard
-        VkPhysicalDeviceFeatures deviceFeatures{};
+		// features, that Im going to use from videocard
+		VkPhysicalDeviceFeatures deviceFeatures{};
 
-        VkDeviceCreateInfo createInfo{};
-        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+		VkDeviceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
 
-        createInfo.pQueueCreateInfos = &queueCreateInfo;
-        createInfo.queueCreateInfoCount = 1;
+		createInfo.pQueueCreateInfos    = &queueCreateInfo;
+		createInfo.queueCreateInfoCount = 1;
 
-        createInfo.pEnabledFeatures = &deviceFeatures;
+		createInfo.pEnabledFeatures = &deviceFeatures;
 
-        std::vector<const char*> deviceExtensions;
+		std::vector<const char *> deviceExtensions;
 
-        if (isMac) {
-            deviceExtensions.push_back(
-                VK_KHR_PORTABILITY_SUBSET_EXTENSION_NAME);
-        }
+		if (isMac)
+		{
+			deviceExtensions.push_back(
+			    VK_KHR_PORTABILITY_SUBSET_EXTENSION_NAME);
+		}
 
-        createInfo.enabledExtensionCount =
-            static_cast<uint32_t>(deviceExtensions.size());
-        createInfo.ppEnabledExtensionNames = deviceExtensions.data();
+		createInfo.enabledExtensionCount =
+		    static_cast<uint32_t>(deviceExtensions.size());
+		createInfo.ppEnabledExtensionNames = deviceExtensions.data();
 
-        if (enableValidationLayers) {
-            createInfo.enabledLayerCount =
-                static_cast<uint32_t>(validationLayers.size());
-            createInfo.ppEnabledLayerNames = validationLayers.data();
-        } else {
-            createInfo.enabledLayerCount = 0;
-        }
+		if (enableValidationLayers)
+		{
+			createInfo.enabledLayerCount =
+			    static_cast<uint32_t>(validationLayers.size());
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+		}
+		else
+		{
+			createInfo.enabledLayerCount = 0;
+		}
 
-        VkDevice device;
+		VkDevice device;
 
-        if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) !=
-            VK_SUCCESS) {
-            throw std::runtime_error("failed to create logical device!");
-        }
+		if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) !=
+		    VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create logical device!");
+		}
 
-        return device;
-    }
+		return device;
+	}
 };
--- main.cpp
+++ main.cpp
@@ -8,180 +8,206 @@
 
 #include "device_helpers.cpp"
 
-const uint32_t WIDTH = 800;
+const uint32_t WIDTH  = 800;
 const uint32_t HEIGHT = 600;
 
-const std::vector<const char*> validationLayers = {
+const std::vector<const char *> validationLayers = {
     "VK_LAYER_KHRONOS_validation"};
 
-class HelloTriangleApplication {
-   public:
-    void run() {
-        initWindow();
-        initVulkan();
-        mainLoop();
-        cleanup();
-    }
+class HelloTriangleApplication
+{
+  public:
+	void run()
+	{
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
 
-   private:
-    GLFWwindow* window;
-    VkInstance instance;
-    VkPhysicalDevice physicalDevice;
-    VkDevice device;
-    VkQueue graphicsQueue;  // queue to the selected logical device
-    VkSurfaceKHR surface;   // surface to render into
-    VkQueue presentQueue;   // presentation qeueue, connected to the surface
+  private:
+	GLFWwindow      *window;
+	VkInstance       instance;
+	VkPhysicalDevice physicalDevice;
+	VkDevice         device;
+	VkQueue          graphicsQueue;        // queue to the selected logical device
+	VkSurfaceKHR     surface;              // surface to render into
+	VkQueue          presentQueue;         // presentation qeueue, connected to the surface
 
-    void mainLoop() {
-        while (!glfwWindowShouldClose(window)) {
-            glfwPollEvents();
-        }
-    }
+	void mainLoop()
+	{
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwPollEvents();
+		}
+	}
 
-    void cleanup() {
-        vkDestroyDevice(device, nullptr);
-        vkDestroySurfaceKHR(instance, surface, nullptr);
-        vkDestroyInstance(instance, nullptr);
+	void cleanup()
+	{
+		vkDestroyDevice(device, nullptr);
+		vkDestroySurfaceKHR(instance, surface, nullptr);
+		vkDestroyInstance(instance, nullptr);
 
-        glfwDestroyWindow(window);
-        glfwTerminate();
-    }
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
 
-    void initWindow() {
-        glfwInit();
+	void initWindow()
+	{
+		glfwInit();
 
-        // disable opengl
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		// disable opengl
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
-        // disable window resizing
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		// disable window resizing
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-    }
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
 
-    void initVulkan() {
-        createInstance();
-        createSurface();
-        physicalDevice = DeviceHelpers::pickPhysicalDevice(instance, surface);
-        device = DeviceHelpers::createLogicalDevice(physicalDevice,
-                                                    validationLayers, surface);
+	void initVulkan()
+	{
+		createInstance();
+		createSurface();
+		physicalDevice = DeviceHelpers::pickPhysicalDevice(instance, surface);
+		device         = DeviceHelpers::createLogicalDevice(physicalDevice,
+		                                                    validationLayers, surface);
 
-        // todo a bit dirty, since it is already called inside
-        // createLogicalDevice
-        QueueFamilyIndices indices =
-            DeviceHelpers::findQueueFamilies(physicalDevice, surface);
-        vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0,
-                         &graphicsQueue);
-    }
+		// todo a bit dirty, since it is already called inside
+		// createLogicalDevice
+		QueueFamilyIndices indices =
+		    DeviceHelpers::findQueueFamilies(physicalDevice, surface);
+		vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0,
+		                 &graphicsQueue);
+	}
 
-    void createSurface() {
-        if (glfwCreateWindowSurface(instance, window, nullptr, &surface) !=
-            VK_SUCCESS) {
-            throw std::runtime_error("failed to create window surface!");
-        }
-    }
+	void createSurface()
+	{
+		if (glfwCreateWindowSurface(instance, window, nullptr, &surface) !=
+		    VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create window surface!");
+		}
+	}
 
-    bool checkValidationLayerSupport() {
-        uint32_t layerCount;
-        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+	bool checkValidationLayerSupport()
+	{
+		uint32_t layerCount;
+		vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
 
-        std::vector<VkLayerProperties> availableLayers(layerCount);
-        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+		std::vector<VkLayerProperties> availableLayers(layerCount);
+		vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
 
-        for (const char* layerName : validationLayers) {
-            bool layerFound = false;
+		for (const char *layerName : validationLayers)
+		{
+			bool layerFound = false;
 
-            for (const auto& layerProperties : availableLayers) {
-                if (strcmp(layerName, layerProperties.layerName) == 0) {
-                    layerFound = true;
-                    break;
-                }
-            }
+			for (const auto &layerProperties : availableLayers)
+			{
+				if (strcmp(layerName, layerProperties.layerName) == 0)
+				{
+					layerFound = true;
+					break;
+				}
+			}
 
-            if (!layerFound) {
-                return false;
-            }
-        }
+			if (!layerFound)
+			{
+				return false;
+			}
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    std::vector<const char*> getExtentions() {
-        uint32_t glfwExtensionCount = 0;
-        const char** glfwExtensions;
+	std::vector<const char *> getExtentions()
+	{
+		uint32_t     glfwExtensionCount = 0;
+		const char **glfwExtensions;
 
-        // required for glfw
-        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		// required for glfw
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-        std::vector<const char*> extensions(
-            glfwExtensions, glfwExtensions + glfwExtensionCount);
+		std::vector<const char *> extensions(
+		    glfwExtensions, glfwExtensions + glfwExtensionCount);
 
-        if (isMac) {
-            extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
-            extensions.push_back(
-                VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME);
-        }
+		if (isMac)
+		{
+			extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+			extensions.push_back(
+			    VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME);
+		}
 
-        return extensions;
-    }
+		return extensions;
+	}
 
-    // The main vulkan settings
-    void createInstance() {
-        if (enableValidationLayers && !checkValidationLayerSupport()) {
-            throw std::runtime_error(
-                "validation layers requested, but not available!");
-        }
+	// The main vulkan settings
+	void createInstance()
+	{
+		if (enableValidationLayers && !checkValidationLayerSupport())
+		{
+			throw std::runtime_error(
+			    "validation layers requested, but not available!");
+		}
 
-        VkApplicationInfo appInfo{};
-        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.pApplicationName = "Hello Triangle";
-        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.pEngineName = "No Engine";
-        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion = VK_API_VERSION_1_0;
+		VkApplicationInfo appInfo{};
+		appInfo.sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName   = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName        = "No Engine";
+		appInfo.engineVersion      = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion         = VK_API_VERSION_1_0;
 
-        // the actual vulkan and its extensions
-        VkInstanceCreateInfo createInfo{};
-        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-        createInfo.pApplicationInfo = &appInfo;
+		// the actual vulkan and its extensions
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType            = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
 
-        auto requiredExtensions = getExtentions();
+		auto requiredExtensions = getExtentions();
 
-        createInfo.enabledExtensionCount =
-            static_cast<uint32_t>(requiredExtensions.size());
+		createInfo.enabledExtensionCount =
+		    static_cast<uint32_t>(requiredExtensions.size());
 
-        // todo MAC OS ONLY
-        createInfo.flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+		// todo MAC OS ONLY
+		createInfo.flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
 
-        createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+		createInfo.ppEnabledExtensionNames = requiredExtensions.data();
 
-        createInfo.enabledLayerCount = 0;
+		createInfo.enabledLayerCount = 0;
 
-        if (enableValidationLayers) {
-            createInfo.enabledLayerCount =
-                static_cast<uint32_t>(validationLayers.size());
-            createInfo.ppEnabledLayerNames = validationLayers.data();
-        } else {
-            createInfo.enabledLayerCount = 0;
-        }
+		if (enableValidationLayers)
+		{
+			createInfo.enabledLayerCount =
+			    static_cast<uint32_t>(validationLayers.size());
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+		}
+		else
+		{
+			createInfo.enabledLayerCount = 0;
+		}
 
-        auto result = vkCreateInstance(&createInfo, nullptr, &instance);
+		auto result = vkCreateInstance(&createInfo, nullptr, &instance);
 
-        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-            throw std::runtime_error("failed to create instance!");
-        }
-    }
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create instance!");
+		}
+	}
 };
 
-int main() {
-    HelloTriangleApplication app;
+int main()
+{
+	HelloTriangleApplication app;
 
-    try {
-        app.run();
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
+	try
+	{
+		app.run();
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
