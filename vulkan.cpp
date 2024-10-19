cf396297f1d500be1731ca0dbe4c34d053948660
Vasin Ilya <ilya.vasin@ringcentral.com>
2024-10-19 15:02:24 +0200

    render pass clean up fixed

--- vulkan.cpp
+++ vulkan.cpp
@@ -34,8 +34,9 @@ class Vulkan
 	}
 	~Vulkan()
 	{
+		vkDestroyPipeline(device, graphicsPipeline, nullptr);
 		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
-		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+		vkDestroyRenderPass(device, renderPass, nullptr);
 
 		for (auto imageView : swapChainImageViews)
 		{
@@ -62,6 +63,7 @@ class Vulkan
 	std::vector<VkImageView> swapChainImageViews;
 	VkRenderPass             renderPass;
 	VkPipelineLayout         pipelineLayout;
+	VkPipeline               graphicsPipeline;
 
 	QueueFamilyIndices indices;
 
@@ -393,6 +395,33 @@ class Vulkan
 		if (vkCreatePipelineLayout(device, &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS)
 		{
 			throw std::runtime_error("failed to create pipeline layout!");
+		}
+
+		// the final pipeline definition
+		VkGraphicsPipelineCreateInfo pipelineInfo{};
+		pipelineInfo.sType      = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+		pipelineInfo.stageCount = 2;
+		pipelineInfo.pStages    = shaderStages;
+
+		pipelineInfo.pVertexInputState   = &vertexInputInfo;
+		pipelineInfo.pInputAssemblyState = &inputAssembly;
+		pipelineInfo.pViewportState      = &viewportState;
+		pipelineInfo.pRasterizationState = &rasterizer;
+		pipelineInfo.pMultisampleState   = &multisampling;
+		pipelineInfo.pDepthStencilState  = nullptr;        // Optional
+		pipelineInfo.pColorBlendState    = &colorBlending;
+		pipelineInfo.pDynamicState       = &dynamicState;
+
+		pipelineInfo.layout = pipelineLayout;
+
+		pipelineInfo.renderPass         = renderPass;
+		pipelineInfo.subpass            = 0;
+		pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;        // Optional
+		pipelineInfo.basePipelineIndex  = -1;                    // Optional
+
+		if (vkCreateGraphicsPipelines(device, VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &graphicsPipeline) != VK_SUCCESS)
+		{
+			throw std::runtime_error("failed to create graphics pipeline!");
 		}
 
 		// modules must be cleaned after use!
