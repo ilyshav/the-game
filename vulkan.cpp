ae20fd874c1a71aea446b67f9f096f5897924114
Ilia Vasin <me@vasin-ilya.com>
2024-10-20 19:25:36 +0200

    draw reworked

--- vulkan.cpp
+++ vulkan.cpp
@@ -76,67 +76,63 @@ class Vulkan
 
 	void drawFrame()
 	{
-		vkWaitForFences(device, 1, &inFlightFences[currentFrame], VK_TRUE, UINT64_MAX);
+		auto r = device.waitForFences(1, &inFlightFences[currentFrame], true, UINT64_MAX);
 
-		uint32_t imageIndex;
-		VkResult result = vkAcquireNextImageKHR(device, swapChain, UINT64_MAX, imageAvailableSemaphores[currentFrame], VK_NULL_HANDLE, &imageIndex);
+		uint32_t   imageIndex;
+		vk::Result result =
+		    device.acquireNextImageKHR(swapChain, UINT64_MAX, imageAvailableSemaphores[currentFrame], VK_NULL_HANDLE, &imageIndex);
 
-		if (result == VK_ERROR_OUT_OF_DATE_KHR)
+		if (result == vk::Result::eErrorOutOfDateKHR)
 		{
 			recreateSwapChain();
 			return;
 		}
-		else if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR)
+		else if (result != vk::Result::eSuccess && result != vk::Result::eSuboptimalKHR)
 		{
 			throw std::runtime_error("failed to acquire swap chain image!");
 		}
 
-		vkResetFences(device, 1, &inFlightFences[currentFrame]);
+		r = device.resetFences(1, &inFlightFences[currentFrame]);
+		// vkResetFences(device, 1, &inFlightFences[currentFrame]);
 
-		vkResetCommandBuffer(commandBuffers[currentFrame], /*VkCommandBufferResetFlagBits*/ 0);
+		commandBuffers[currentFrame].reset();
 		recordCommandBuffer(commandBuffers[currentFrame], imageIndex);
 
-		VkSubmitInfo submitInfo{};
-		submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+		auto submitInfo = vk::SubmitInfo();
 
-		VkSemaphore          waitSemaphores[] = {imageAvailableSemaphores[currentFrame]};
-		VkPipelineStageFlags waitStages[]     = {VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
-		submitInfo.waitSemaphoreCount         = 1;
-		submitInfo.pWaitSemaphores            = waitSemaphores;
-		submitInfo.pWaitDstStageMask          = waitStages;
+		vk::Semaphore          waitSemaphores[] = {imageAvailableSemaphores[currentFrame]};
+		vk::PipelineStageFlags waitStages[]     = {vk::PipelineStageFlagBits::eColorAttachmentOutput};
+		submitInfo.waitSemaphoreCount           = 1;
+		submitInfo.pWaitSemaphores              = waitSemaphores;
+		submitInfo.pWaitDstStageMask            = waitStages;
 
 		submitInfo.commandBufferCount = 1;
 		submitInfo.pCommandBuffers    = &commandBuffers[currentFrame];
 
-		VkSemaphore signalSemaphores[]  = {renderFinishedSemaphores[currentFrame]};
-		submitInfo.signalSemaphoreCount = 1;
-		submitInfo.pSignalSemaphores    = signalSemaphores;
+		vk::Semaphore signalSemaphores[] = {renderFinishedSemaphores[currentFrame]};
+		submitInfo.signalSemaphoreCount  = 1;
+		submitInfo.pSignalSemaphores     = signalSemaphores;
 
-		if (vkQueueSubmit(graphicsQueue, 1, &submitInfo, inFlightFences[currentFrame]) != VK_SUCCESS)
-		{
-			throw std::runtime_error("failed to submit draw command buffer!");
-		}
+		graphicsQueue.submit(submitInfo, inFlightFences[currentFrame]);
 
-		VkPresentInfoKHR presentInfo{};
-		presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
-
+		auto presentInfo               = vk::PresentInfoKHR();
 		presentInfo.waitSemaphoreCount = 1;
 		presentInfo.pWaitSemaphores    = signalSemaphores;
 
-		VkSwapchainKHR swapChains[] = {swapChain};
-		presentInfo.swapchainCount  = 1;
-		presentInfo.pSwapchains     = swapChains;
+		vk::SwapchainKHR swapChains[] = {swapChain};
+		presentInfo.swapchainCount    = 1;
+		presentInfo.pSwapchains       = swapChains;
 
 		presentInfo.pImageIndices = &imageIndex;
 
-		result = vkQueuePresentKHR(presentQueue, &presentInfo);
+		auto pResult = presentQueue.presentKHR(presentInfo);
 
-		if (result == VK_ERROR_OUT_OF_DATE_KHR || result == VK_SUBOPTIMAL_KHR || framebufferResized)
+		if (pResult == vk::Result::eErrorOutOfDateKHR || pResult == vk::Result::eSuboptimalKHR || framebufferResized)
 		{
 			framebufferResized = false;
 			recreateSwapChain();
 		}
-		else if (result != VK_SUCCESS)
+		else if (result != vk::Result::eSuccess)
 		{
 			throw std::runtime_error("failed to present swap chain image!");
 		}
@@ -165,7 +161,7 @@ class Vulkan
 	// rendering related
 	std::vector<VkSemaphore> imageAvailableSemaphores;
 	std::vector<VkSemaphore> renderFinishedSemaphores;
-	std::vector<VkFence>     inFlightFences;
+	std::vector<vk::Fence>   inFlightFences;
 
 	QueueFamilyIndices indices;
 
@@ -570,12 +566,9 @@ class Vulkan
 
 		for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++)
 		{
-			if (vkCreateSemaphore(device, &semaphoreInfo, nullptr, &imageAvailableSemaphores[i]) != VK_SUCCESS ||
-			    vkCreateSemaphore(device, &semaphoreInfo, nullptr, &renderFinishedSemaphores[i]) != VK_SUCCESS ||
-			    vkCreateFence(device, &fenceInfo, nullptr, &inFlightFences[i]) != VK_SUCCESS)
-			{
-				throw std::runtime_error("failed to create synchronization objects for a frame!");
-			}
+			imageAvailableSemaphores[i] = device.createSemaphore(semaphoreInfo);
+			renderFinishedSemaphores[i] = device.createSemaphore(semaphoreInfo);
+			inFlightFences[i]           = device.createFence(fenceInfo);
 		}
 	}
 
